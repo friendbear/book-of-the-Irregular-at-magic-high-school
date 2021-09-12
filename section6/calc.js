f3b2f73713e745c3514c2bf09be26effa05f1031
T Kumagai <kumagai@bear.plala.or.jp>
2021-09-12 15:11:42 +0900

    section6

---
+++ section6/calc.js
@@ -0,0 +1,17 @@
+var x = 0;
+document.writeln(x);
+
+var s = 'Javascriptの変数';
+document.writeln(s);
+
+var before = 100000;
+var mul = 1.06 ** 10;
+var after = before * mul;
+var answer = after - before;
+document.writeln(answer);
+
+// このコードは三角形の面積を求めるプログラムです
+var w = 3;
+var h = 10;
+var area = ( w * h) / 2;
+document.writeln(area); // 面積を出力する
